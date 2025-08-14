f5a4bca62b399fe36775a1312fda8812f34086fd
Dropper08 <dropperNFTS@gmail.com>
2025-08-14 10:37:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -165,21 +165,20 @@ try:
                             LIMIT 2
                         """), {'video_id': video_id}
                     ).fetchall()
-
-                    if len(last_two_rows) == 2 and (current_views != previous_views):
+                    
+                    views_diff = 0
+                    delta = 0
+                    pace_per_hour = 0
+                    pace_24h = 0
+                    
+                    if len(last_two_rows) == 2:
                         current_views, current_time = last_two_rows[0]
                         previous_views, previous_time = last_two_rows[1]
-                        
-                        views_diff = views - current_views
-                        delta = (views_diff / (current_views - previous_views)) - 1
-                        pace_per_hour = (views_diff / WAIT) * 60
-                        # pace_24h = pace_per_hour * 24
-
-                    else:
-                        views_diff = 0
-                        delta = 0
-                        pace_per_hour = 0
-                        pace_24h = 0
+                        if (current_views != previous_views):
+                            views_diff = views - current_views
+                            delta = (views_diff / (current_views - previous_views)) - 1
+                            pace_per_hour = (views_diff / WAIT) * 60
+                            # pace_24h = pace_per_hour * 24
 
                     print(f'[{agora_brasilia.strftime("%Y-%m-%d %H:%M:%S")}] {video_id}: {views} views (+{views_diff} desde a última atualização)')
 
