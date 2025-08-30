91ee24f95111d17e2b2254731a54c4e818bf861c
Dropper08 <dropperNFTS@gmail.com>
2025-08-30 13:02:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ DATABASE_URL = API_KEY = os.getenv("DATABASE_URL")  # coloque seus dados aqui
 
 # 🎥 Lista dos vídeos que você quer monitorar
 VIDEOS = [
-    {'video_id': 'M2lX9XESvDE', 'titulo': 'Taylor Swift'}
+    {'video_id': '_AbFXuGDRTs', 'titulo': 'Bunker'}
 ]
 
 # 🔑 API KEY do YouTube
